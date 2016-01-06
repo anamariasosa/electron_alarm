cd3c21cdef907038dac48be32b88a5c31cd72f41
anamariasosam <anamariasosam@gmail.com>
2016-01-06 17:31:06 -0500

    electron app file

---
+++ window.js
@@ -0,0 +1,75 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var Menu = require("menu");
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    icon: '/src/icons/icon.png',
+    width: 300,
+    height: 160,
+    frame: false,
+    resizable: false,
+    transparent: false
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/src/index.html');
+
+
+  var menu = new Menu();
+
+  var template = [];
+
+  if (process.platform == 'darwin') {
+    var name = app.getName();
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          label: 'About ' + name,
+          role: 'about'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() { app.quit(); }
+        },
+      ]
+    });
+  }
+
+  menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+
+ //  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
